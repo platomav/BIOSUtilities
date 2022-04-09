96e87455deffc57ceae21f52f5c0787c78f3b12a
platomav <platomav@users.noreply.github.com>
2022-04-09 22:27:47 +0300

    Small AMI UCP pattern improvement

--- common/patterns.py
+++ common/patterns.py
@@ -4,7 +4,7 @@
 import re
 
 PAT_AMI_PFAT = re.compile(b'_AMIPFAT.AMI_BIOS_GUARD_FLASH_CONFIGURATIONS', re.DOTALL)
-PAT_AMI_UCP = re.compile(br'@(UAF|HPU).{12}\x40', re.DOTALL)
+PAT_AMI_UCP = re.compile(br'@(UAF|HPU).{12}@', re.DOTALL)
 PAT_DELL_PKG = re.compile(br'\x72\x13\x55\x00.{45}\x37\x7A\x58\x5A', re.DOTALL)
 PAT_DELL_HDR = re.compile(br'\xEE\xAA\x76\x1B\xEC\xBB\x20\xF1\xE6\x51.\x78\x9C', re.DOTALL)
 PAT_DELL_FTR = re.compile(br'\xEE\xAA\xEE\x8F\x49\x1B\xE8\xAE\x14\x37\x90')
